e88111b840ab7797c12b049793b680632736888e
qwest812 <qwest812@yandex.ru>
2017-05-14 19:25:11 +0300

    task 26/04

---
+++ js/home task 26.04.2017/task.php
@@ -0,0 +1,100 @@
+<html>
+<script type="text/javascript">
+
+
+
+//var first=parseInt(prompt("ВВедите первое значение"));
+//var second=parseInt(prompt("ВВедите второе значение"));
+//var res=Number(first) + second;
+//alert(res);
+// first=prompt("ВВедите первое значение","0");
+// second=prompt("ВВедите второе значение","0");
+//alert(+first*second);
+var a=null;
+var b=undefined;
+var c=NaN;
+var d=0;
+var e="";
+
+    if(a==b){
+        alert(true);
+    }else {
+        alert(false)
+    }
+
+if(a==c){
+    alert(true);
+}else {
+    alert(false)
+}
+
+if(a==d){
+    alert(true);
+}else {
+    alert(false)
+}
+
+if(a==e){
+    alert(true);
+}else {
+    alert(false)
+}
+
+if(b==c){
+    alert(true);
+}else {
+    alert(false)
+}
+
+if(b==d){
+    alert(true);
+}else {
+    alert(false)
+}
+
+if(b==e){
+    alert(true);
+}else {
+    alert(false)
+}
+
+
+if(c==d){
+    alert(true);
+}else {
+    alert(false)
+}
+
+if(c==e){
+    alert(true);
+}else {
+    alert(false)
+}
+///if(b==d){
+//    alert(true);
+//}else {
+//    alert(false)
+//}
+//if(b==d){
+//    alert(true);
+//}else {
+//    alert(false)
+//}
+//if(b==d){
+//    alert(true);
+//}else {
+//    alert(false)
+//}
+//if(b==d){
+//    alert(true);
+//}else {
+//    alert(false)
+//}
+//if(b==d){
+//    alert(true);
+//}else {
+//    alert(false)
+//}
+//
+</script>
+</html>
