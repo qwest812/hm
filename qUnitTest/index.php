9db92d1d42a9b71b21ec0a1d3d6bcab4d807d8a6
qwest812 <qwest812@yandex.ru>
2017-08-07 10:27:12 +0300

    newTask(26,28)

---
+++ qUnitTest/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC-1
+ * Date: 19.07.2017
+ * Time: 15:57
+ */
---
+++ qUnitTest/tests.js
@@ -0,0 +1,6 @@
+/**
+ * Created by PC-1 on 19.07.2017.
+ */
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
