e33fd5488ae5206122aeb76f98412eddc1613432
qwest812 <qwest812@yandex.ru>
2017-08-09 18:04:28 +0300

    02.08.2017 v1.2

--- newTask/02.08.2017/main.js
+++ newTask/02.08.2017/main.js
@@ -1,46 +1,38 @@
-var ourRequest =new XMLHttpRequest;
-var btn= document.getElementById('btn');
-var animalContainer=document.getElementById("animal-info");
-var click=1;
-btn.addEventListener("click",function(){
-
-ourRequest.open("GET","http://education/hm/newTask/02.08.2017/JSON.json");
-ourRequest.onload=function(){
-    //console.log(JSON.stringify(ourRequest.responseText));
-    var ourData= JSON.parse(ourRequest.responseText);
-    //console.log(ourData);
-    //console.log(JSON.stringify(ourRequest.responseText));
-    //console.log(ourData[0]);
-    render(ourData);
-};
+var ourRequest = new XMLHttpRequest;
+var btn = document.getElementById('btn');
+var animalContainer = document.getElementById("animal-info");
+var click = 0;
+btn.addEventListener("click", function () {
+    ourRequest.open("GET", "http://education/hm/newTask/02.08.2017/JSON.json");
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        render(ourData);
+    };
     ourRequest.send();
 
 });
+function render(data) {
+    var lengh = data.length;
+    var renderHTML = "";
 
-
-function render(data){
-    var lengh=data.length;
-    var renderHTML="";
-
-    for(var i=0; i<lengh;i++){
-        if(i==click){
-        for(var name in data[i]){
-
-                        renderHTML+=data[i][name]+"---";
-
-                    }
-            renderHTML+="<br>";
+    for (var i = 0; i < lengh; i++) {
+        if (i == click) {
+            for (var name in data[i]) {
+                renderHTML += data[i][name] + "---";
+            }
+            renderHTML += "<br>";
         }
-
-
-        }
-    console.log(click);
-    console.log(lengh)
-    if(click>lengh){
-                click=0;
     }
-    click++;
-animalContainer.insertAdjacentHTML('beforeend',renderHTML);
+    console.log(click);
+    console.log(lengh);
+    if (click < lengh) {
+        click++;
+        if(click>lengh ||click==lengh){
+            click=0;
+        }
+    }
+
+    animalContainer.insertAdjacentHTML('beforeend', renderHTML);
 }
 /**
  * Created by PC-1 on 08.08.2017.
